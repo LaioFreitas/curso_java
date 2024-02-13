3b9b75880b0185e74ff256db6e64956906bdaf74
LaioFreitas <laiofrsitas447@gmail.com>
2024-02-13 00:25:05 -0300

    execoes-solucao-ruim

--- criando_excecoes/src/App.java
+++ criando_excecoes/src/App.java
@@ -3,6 +3,8 @@ import java.text.SimpleDateFormat;
 import java.util.Locale;
 import java.util.Scanner;
 
+import model.entites.Reservation;
+
 public class App {
     public static void main(String[] args) throws Exception {
         Locale.setDefault(Locale.US);
@@ -15,5 +17,35 @@ public class App {
 
         System.out.print("check-in date (dd/MM/yyyy)");
         Date checkIn = sdf.parse(sc.next());
+
+        System.out.print("check-out date (dd/MM/yyyy)");
+        Date checkOut = sdf.parse(sc.next());
+
+        if (!checkOut.after(checkIn)) {
+            System.out.println("error in resevertion: checkOut date must be after ");
+        }
+        else {
+            Reservation reservation = new Reservation(number, checkIn, checkOut);
+            System.out.println("reservation " + reservation);
+            
+            System.out.println();
+            System.out.println("enter data update the resevation: ");
+            System.out.print("check-in date (dd/MM/yyyy): ");
+            checkIn = sdf.parse(sc.next());
+            System.out.print("check-out date (dd/MM/yyyy)");
+            checkOut = sdf.parse(sc.next());
+            
+            Date now = new Date();
+            if (checkIn.before(now) || checkOut.before(now)) {
+                System.out.println("error in reservation: reservation dates for  update must be future");
+            }
+            else if (!checkOut.after(checkIn)) {
+                System.out.println("error in resevertion: checkOut date must be after ");
+            }
+            else {
+                reservation.updateDates(checkIn, checkOut);
+            }
+        }
+        sc.close();
     }
 }
